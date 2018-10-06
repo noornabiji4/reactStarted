799d7ddf21eed093702d6aed21f4b05c3e27a7a9
Noor <noornabiji4@gmail.com>
2018-10-06 17:34:22 +0530

    componant

--- app/friends.js
+++ app/friends.js
@@ -1,13 +1,35 @@
 import React from 'react'
 
 //function componants 
-const Friend = function(props){
+class Friend extends React.Component{
+render(){
+var friend= this.props.friends.filter((friends)=> friends.friend === true)
+var nonFriend = this.props.friends.filter((friends)=> friends.friend !== true)
     return(
-        <ol>
-        {props.friends.map((friends)=><li key={friends.id}>{friends.name}</li>
-           )}
-        </ol>
+            <div>
+                <h1>friends</h1>
+        {friend.map((friends)=>{
+            return <li key={friends.id}>{friends.name}</li>
+        })}
+
+        <hr/>
+        <h1>non friends</h1>
+        {nonFriend.map((friends)=>{
+            return <li key={friends.id}>{friends.name}</li>
+
+        })}
+        
+
+
+
+
+        </div>
+        // <ol>
+        // {props.friends.map((friends)=><li key={friends.id}>{friends.name}</li>
+        //    )}
+        // </ol>
     )
+}
 }
 
 export default Friend

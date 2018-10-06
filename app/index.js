dda5513c4ce7cfeb0efb2b0475d16c9f6df587c7
Noor <noornabiji4@gmail.com>
2018-10-06 17:34:58 +0530

    chenges

--- app/index.js
+++ app/index.js
@@ -4,19 +4,53 @@
 //react dom
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Car from './car.js';
 import Friend from './friends.js'
+
 require('./index.css');
 
+// const cars=[
+//     {id : 1, name : "Benz"},
+//     {id : 2, name : " Audi" },
+//     {id : 3, name : "Rolls Royce"}
+// ]
 const friends=[
-    {id: 1 , name : "misbah"},
-    {id : 2,name : " imad" },
-    {id : 3, name : "afzal"}
+    {id : 1, name : "misbah", friend:true},
+    {id : 2,name : " imad" , friend:true },
+    {id : 3, name : "afzal" , friend:true},
+    {id : 4, name : "aa", friend:false},
+    {id : 5,name : " shoeb" , friend:true },
+    {id : 6, name : "bb" , friend:false}
 ]
+
+
+
 
 class ListFriend extends React.Component{
     render(){
+       
         return(
-           <Friend  friends={friends}/>
+
+            
+            // friends.filter((friends)=>{
+            //     if(friends === true){
+            //         return true;
+            //         friends.map((friends)=>{
+            //             return <ul> <ol key={friends.id}>Ture friends{friends}</ol></ul>
+            //         })
+            //         friends.filter((friends)=>{
+            //             if(friends === false){
+            //                 return false;
+            //                 friends.map((friends)=>{
+            //                     return <ul> <ol key={friends.id}>flase friends{friends}</ol></ul>
+                                
+            //                   
+                            
+            //                 })
+            //             }
+            //         }
+            //     })
+            <Friend  friends={friends}/>
         )
     }
     
@@ -29,3 +63,33 @@ class ListFriend extends React.Component{
         
     )
     
+
+
+
+
+
+
+
+
+
+
+
+
+// class App extends React.Component{
+//     render(){
+//         return(
+//            <Car cars={cars}/>
+//         )
+//     }
+    
+//     }
+    
+//     ReactDOM.render(
+//         <App/>,
+      
+//         document.getElementById('content')
+        
+//     )
+    
+
+
